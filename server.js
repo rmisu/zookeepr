0a8fe96ba14830d9af56ae81014a4e3627508b5d
rmisu <roger889@umn.edu>
2022-07-16 04:10:17 -0500

    changed PORT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { restoreDefaultPrompts } = require('inquirer');
 const app = express();
 const { animals } = require('./data/animals');
+const PORT = process.env.PORT || 3001;
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -37,6 +38,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log('API server now on port 3001!');
 });
